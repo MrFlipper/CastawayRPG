ecbe1efe58e0022727f3633d1a8df3b69d3d93c7
MrFlipper <rohinsingh2@gmail.com>
2015-05-05 01:50:43 -0700

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,13 @@
+package CastawayRPG;
+
+public Driver {
+	public static void main(String Args[]){
+int i=0;
+		Select player = new Select();
+		player.confirm();
+		for(i=0; i<10; i++){		
+		Skilltest game = new Skilltest();
+		}
+
+	}
+}
